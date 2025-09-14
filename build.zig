31c7f9940777f65d0f81d237223c111fcd28464a
KodinGoose <baronovicsbarnabas@gmail.com>
2025-09-15 01:08:47 +0200

    Delete abandoned gui

--- build.zig
+++ build.zig
@@ -39,20 +39,10 @@ pub fn build(b: *std.Build) void {
         .strip = if (optimize == .Debug) false else true,
         .link_libc = true,
     });
-    const gui_module = b.createModule(.{
-        .root_source_file = b.path("gui/main.zig"),
-        .target = target,
-        .optimize = optimize,
-        .single_threaded = if (optimize == .Debug) false else true,
-        .strip = if (optimize == .Debug) false else true,
-        .link_libc = true,
-    });
 
     assembler_module.addImport("shared", shared_module);
     interpreter_module.addImport("shared", shared_module);
     interpreter_module.addImport("sdl_bindings", sdl_bindings_module);
-    gui_module.addImport("shared", shared_module);
-    gui_module.addImport("sdl_bindings", sdl_bindings_module);
 
     const assembler = b.addExecutable(.{
         .name = "assembler",
@@ -62,33 +52,20 @@ pub fn build(b: *std.Build) void {
         .name = "interpreter",
         .root_module = interpreter_module,
     });
-    const gui = b.addExecutable(.{
-        .name = "gui",
-        .root_module = gui_module,
-    });
 
     if (target.result.os.tag == .windows) {
-        interpreter_module.addLibraryPath(b.path("windows_sdl/lib/"));
         interpreter_module.addLibraryPath(b.path("windows_sdl/bin/"));
-        gui_module.addLibraryPath(b.path("windows_sdl/lib/"));
-        gui_module.addLibraryPath(b.path("windows_sdl/bin/"));
     }
 
     interpreter.linkSystemLibrary("SDL3");
-    gui.linkSystemLibrary("SDL3");
-    gui.linkSystemLibrary("SDL3_ttf");
 
     b.installArtifact(assembler);
     b.installArtifact(interpreter);
-    b.installArtifact(gui);
     if (target.result.os.tag == .windows) {
         b.installFile("windows_sdl/bin/SDL3.dll", "bin/SDL3.dll");
-        b.installFile("windows_sdl/bin/SDL3_ttf.dll", "bin/SDL3_ttf.dll");
     }
     b.installFile("interpreter/starting_memory", "bin/starting_memory");
     b.installFile("interpreter/beep.wav", "bin/beep.wav");
-    b.installFile("gui/font/Acme 9 Regular Xtnd.ttf", "bin/font/Acme 9 Regular Xtnd.ttf");
-    b.installFile("gui/font/attribution.txt", "bin/font/attribution.txt");
 
     const assembler_unit_tests = b.addTest(.{ .root_module = assembler_module });
 
